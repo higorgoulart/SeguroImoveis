ece69afaa4afffe3c8c86eb0700cb3f2fe8a4bbc
Guilherme <110054084+guilherme-savio@users.noreply.github.com>
2023-06-28 09:31:30 -0300

    Adicionando trigger para evitar pagamentos em apólices vencidas

--- scripts/bd.sql
+++ scripts/bd.sql
@@ -28,3 +28,17 @@ SELECT c.nome,
  where a.dt_inicio between @dataini and @datafim
 
 END
+
+-- Trigger para evitar o pagamento caso a apólice já esteja cancelada:
+CREATE TRIGGER bloquear_pagamento_cancelado
+BEFORE INSERT ON pagamento
+FOR EACH ROW
+BEGIN
+  DECLARE apolice_cancelada INT;
+  SET apolice_cancelada = (SELECT COUNT(*) FROM apolice WHERE id_apolice = NEW.id_apolice AND dt_termino < CURDATE());
+  IF apolice_cancelada > 0 THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Não é possível inserir pagamento para uma apólice expirada';
+  END IF;
+END;
+
