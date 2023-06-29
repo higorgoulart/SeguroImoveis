f65c29343121cb969ed2d53545d84f65b51659f9
Bruno Frelo Venturini <78036108+Bruno-Venturini@users.noreply.github.com>
2023-06-29 14:38:04 -0300

    Alterando Proc de relatório

--- scripts/bd.sql
+++ scripts/bd.sql
@@ -1,33 +1,30 @@
 --Procedure responsável pelo relatório do window
 DELIMITER $$
-CREATE PROCEDURE sp_relatorio1
-    @dataini DATE,
-    @datafim DATE
-AS
-BEGIN
 
-SELECT c.nome,
-       a.dt_inicio as datainicial,
-       a.dt_termino as datafinal,
-       a.valor_apolice,
-       i.id_imovel,
-       CASE WHEN EXISTS (SELECT 1 FROM sinistro s WHERE s.id_apolice = a.id_apolice) THEN 'Sim' ELSE 'Não' END AS tem_sinistro,
-       (
-              SELECT STUFF(
-              (
-                     SELECT ', ' + co.descricao
-                       FROM cobertura co
-                      INNER JOIN apolice_cobertura ac ON ac.id_cobertura = co.id_cobertura
-                      WHERE ac.id_apolice = a.id_apolice
-                        FOR XML PATH('')
-		), 1, 2, '')
-	   ) AS coberturas,
-       CASE WHEN getdate() between a.dt_inicio and a.dt_termino then 'Sim' ELSE 'Não' END as esta_vigente	
-  FROM cliente c	
-  join imovel i on i.id_proprietario = c.id_cliente
-  join apolice a on a.id_imovel = i.id_imovel
- where a.dt_inicio between @dataini and @datafim
-	
+CREATE PROCEDURE sp_relatorio1 (
+	IN dataini DATE,
+    IN datafim DATE
+)
+BEGIN
+	SELECT
+		c.nome,
+		a.dt_inicio AS datainicial,
+        a.dt_termino AS datafinal,
+        a.valor_apolice,
+        i.id_imovel,
+        (CASE WHEN EXISTS (SELECT COUNT(*) FROM sinistro s WHERE s.id_apolice = a.id_apolice) THEN 'Sim' ELSE 'Não' END) AS tem_sinistro,
+        (
+            SELECT GROUP_CONCAT(co.descricao SEPARATOR ', ')
+            FROM cobertura co
+            INNER JOIN apolice_cobertura ac ON ac.id_cobertura = co.id_cobertura
+            WHERE ac.id_apolice = a.id_apolice
+		) AS coberturas,
+        (CASE WHEN CURDATE() BETWEEN a.dt_inicio AND a.dt_termino THEN 'Sim' ELSE 'Não' END) AS esta_vigente
+	FROM cliente c
+	JOIN imovel i ON i.id_proprietario = c.id_cliente
+	JOIN apolice a ON a.id_imovel = i.id_imovel
+	WHERE a.dt_inicio BETWEEN dataini AND datafim;
+END;	
 END$$
 
 -- Trigger para evitar o pagamento caso a apólice já esteja expirada:
